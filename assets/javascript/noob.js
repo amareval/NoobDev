581c6468dd00f783825f5e80b0b101f27d76876d
brandonao <brandonowensgd@gmail.com>
2018-04-11 21:23:10 -0700

    assets/javascript/noob.js

--- assets/javascript/noob.js
+++ assets/javascript/noob.js
@@ -8,3 +8,17 @@ var config = {
     messagingSenderId: "207322281576"
   };
   firebase.initializeApp(config);
+
+// Create the variable to reference the database
+var database = firebase.database();
+
+// At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
+
+// This callback keeps the page updated when a value changes in firebase.
+database.ref().on("value", function (snapshot) {
+
+// We are now inside our .on function...
+
+console.log(snapshot.val());
+
+});
